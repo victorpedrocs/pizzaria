ed4c3e09613275626c4ee7de033066cf1d1ec017
Victor Pedro <vpedro@coringa.(none)>
2014-07-02 14:48:01 -0300

    teste de cadastra cardapio e pedido

--- src/test/SistemaMock.java
+++ src/test/SistemaMock.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -21,4 +22,36 @@ public class SistemaMock {
 		comandoSQL.executeUpdate(sql);
 	}
 
+	public static void cadastra_pizza() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		Connection conexao;
+		Statement comandoSQL;
+		
+		String nome_pizza = "bacon", ingredientes = "bacon", preco = "22";
+		String sql = "INSERT INTO CARDAPIO_TESTE VALUES('"+nome_pizza+"', '"+ingredientes+"', '"+preco+"')";
+		Class.forName("org.postgresql.Driver").newInstance();
+		
+		conexao = DriverManager.getConnection("jdbc:postgresql://localhost/pizzaria", "postgres", "postgres");
+
+		comandoSQL = conexao.createStatement();
+		comandoSQL.executeUpdate(sql);
+	}
+	
+	public static void cadastrar_pedido() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		Connection conexao;
+		Statement comandoSQL;
+		ResultSet resultado;
+		
+		String telefone = "teste", nome_pizza = "teste";
+		int quantidade = 2;
+		
+		Class.forName("org.postgresql.Driver").newInstance();
+
+		conexao = DriverManager.getConnection("jdbc:postgresql://localhost/pizzaria", "postgres", "postgres");
+
+		comandoSQL = conexao.createStatement();
+	
+		comandoSQL.executeUpdate("INSERT INTO PEDIDO_TESTE VALUES('"+telefone+"', current_timestamp, '"+nome_pizza+"', "+quantidade+")");
+		
+	
+	}
 }
--- src/test/SistemaTest.java
+++ src/test/SistemaTest.java
@@ -1,17 +1,19 @@
 package test;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
+
+import com.sun.crypto.provider.RSACipher;
 
 
 public class SistemaTest {
@@ -26,9 +28,26 @@ public class SistemaTest {
 		stmt = conn.createStatement();
 		
 		StringBuilder sql = new StringBuilder();
-		sql.append("create table cliente_teste (telefone varchar primary key, nome varchar, endereco varchar)");
+		sql.append("create table cliente_teste (telefone varchar primary key, nome varchar, endereco varchar); ");
+		sql.append("create table cardapio_teste (nome_pizza varchar primary key, ingredientes varchar, preco varchar); ");
+		sql.append("create table pedido_teste (telefone varchar,"); 
+			sql.append("data_hora timestamp, ");
+			sql.append("nome_pizza varchar, ");
+			sql.append("quantidade int, ");
+			sql.append("constraint pk_teste primary key (telefone, data_hora, nome_pizza), "); 
+			sql.append("constraint fk_cliente_teste foreign key (telefone) references cliente_teste (telefone), "); 
+			sql.append("constraint fk_cardapio_teste foreign key (nome_pizza) references cardapio_teste(nome_pizza)); ");
 		stmt.executeUpdate(sql.toString());
 		
+		
+		sql = new StringBuilder("INSERT INTO cliente_teste VALUES('teste', 'teste', 'teste'); ");
+		String nome_pizza = "teste", ingredientes = "teste", preco = "teste";
+		sql.append("INSERT INTO CARDAPIO_TESTE VALUES('")
+			.append(nome_pizza).append("', '")
+			.append(ingredientes).append("', '")
+			.append(preco).append("')");
+		
+		stmt.executeUpdate(sql.toString());
 		conn.close();
 	}
 	
@@ -53,18 +72,72 @@ public class SistemaTest {
 		
 		Assert.assertEquals(true, rs.next());
 		
+		rs.close();
 		conn.close();
 	}
 	
-	@After
-	public void afterTest() throws SQLException{
+	@Test
+	public void testCadastrarPizza() throws SQLException{
+		try {
+			SistemaMock.cadastra_pizza();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Statement statement;
+		
+		conn = DriverManager.getConnection("jdbc:postgresql://localhost/pizzaria", "postgres", "postgres");
+		statement = conn.createStatement();
+		
+		StringBuilder sql = new StringBuilder();
+		sql.append("select count(*) from CARDAPIO_TESTE where nome_pizza = 'bacon' and ingredientes = 'bacon' and preco = '22'");
+		
+		ResultSet rs = statement.executeQuery(sql.toString());
+		
+		Assert.assertEquals(true, rs.next());
+		
+		rs.close();
+		conn.close();
+	}
+	
+	@Test
+	public void testPedido() throws SQLException{
+		try {
+			SistemaMock.cadastrar_pedido();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Statement statement;
+		String telefone = "teste", nome_pizza = "teste";
+		int quantidade = 2;
+		
+		conn = DriverManager.getConnection("jdbc:postgresql://localhost/pizzaria", "postgres", "postgres");
+		statement = conn.createStatement();
+		
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT COUNT(*) FROM PEDIDO_TESTE WHERE telefone = '"+telefone+"'and nome_pizza = '"+nome_pizza+"' and quantidade = "+quantidade+";");
+		
+		ResultSet rs = statement.executeQuery(sql.toString());
+		
+		Assert.assertEquals(true, rs.next());
+		
+		rs.close();
+		conn.close();
+	}
+	
+	@AfterClass
+	public static void afterTest() throws SQLException{
 		Statement stmt;
 		
 		conn = DriverManager.getConnection("jdbc:postgresql://localhost/pizzaria", "postgres", "postgres");
 		stmt = conn.createStatement();
 		
 		StringBuilder sql = new StringBuilder();
-		sql.append("drop table cliente_teste;");
+		sql.append("drop table if exists pedido_teste; ")
+			.append("drop table if exists cliente_teste; ")
+			.append("drop table if exists cardapio_teste; ");
+		
 		
 		stmt.executeUpdate(sql.toString());
 		conn.close();
