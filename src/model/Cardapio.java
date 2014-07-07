62927ae52e6728a69a8bcd9b3894ae71c637aa40
vpcsilva <vpcx5@yahoo.com.br>
2014-07-07 11:48:00 -0300

    Refatoracao do sistema legado

---
+++ src/model/Cardapio.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Cardapio {
+	
+	private String nomePizza, ingredientes, preco;
+	
+	public Cardapio(String nomePizza, String ingredientes, String preco){
+		this.nomePizza = nomePizza;
+		this.ingredientes = ingredientes;
+		this.preco = preco;
+	}
+	
+
+}
---
+++ src/model/Cliente.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Cliente {
+
+	private String telefone, nome, endereco;
+	
+	public Cliente(String telefone, String nome, String endereco){
+		this.telefone = telefone;
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+	
+}
