f9d78df159337606ac70221db0a1754f1da35a0a
vpcsilva <vpcx5@yahoo.com.br>
2014-07-08 10:11:23 -0300

    Adicionando DAO e connectionFActory

---
+++ src/model/CardapioDAO.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Connection;
+
+public class CardapioDAO {
+	
+	private Connection connection;
+	
+	public CardapioDAO( Connection con ) {
+		this.connection = con;
+	}
+	
+	
+
+}
